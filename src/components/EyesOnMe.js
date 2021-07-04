757cbdf15b07ab103badafa4b682edfb1e778f5a
Luis Martinez <luis_mmartinez@live.com>
2021-07-04 15:51:27 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+
+    focusPrinter = () =>{
+        console.log("Good!")
+    }
+
+    blurPrinter = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return (
+            <button 
+                onFocus= {this.focusPrinter}
+                onBlur= {this.blurPrinter}
+            >Button
+            </button>
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component {
+
+    keyFunction = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return(
+            <input onKeyUp = {this.keyFunction} type="password"></input>
+        )
+    }
+}
